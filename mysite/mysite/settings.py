a6eda27c7f368d6d3658abfb0f3a846e2ecbdeb6
Jefferson Fong <jefferso@ualberta.ca>
2023-10-23 16:03:37 -0600

    does it work????

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku # top of the file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+django_on_heroku.settings(locals()) # bottom of the file
